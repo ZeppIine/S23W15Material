4ec8f294a7674b0d14bea3c47628050ff68b255a
ZeppIine <kim0904min@gmail.com>
2023-12-14 15:27:49 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w15material/MainActivity.kt
@@ -0,0 +1,30 @@
+package kr.ac.kumoh.ce.s20180147.s23w15material
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.ac.kumoh.ce.s20180147.s23w15material.SongDrawer
+import kr.ac.kumoh.ce.s20180147.s23w15material.ui.theme.S23W15MaterialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    S23W15MaterialTheme {
+        SongDrawer()
+    }
+}
